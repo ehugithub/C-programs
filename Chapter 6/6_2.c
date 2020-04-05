bcc5b7ceb948c5ebe7164f46c1e18dbddc615a63
Eric Hu <erichu03@gmail.com>
2020-04-04 22:36:16 -0400

    edit

--- Chapter 6/6_2.c
+++ Chapter 6/6_2.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 int main() {
-  int num1, num2, gcf, rem;
+  int num1, num2, gcf, temp, rem;
 
   printf("This program finds the GCF between two integer inputs\n");
   printf("Enter two integers, separated by a space: ");
   scanf("%d %d", &num1, & num2);
+  if (num2 < num1) {
+    temp = num2;
+    num2 = num1;
+    num1 = temp;
+}
   for(;;){
-    if (num1 == 0 || num2 == 0)
-      if (num1 == 0)
-        num2 = gcf;
-      else
-        num1 = gcf;
+    if (num1 == 0) {
+      gcf = num2;
       break;
-    if (num1 > num2)
-      rem = num1 % num2;
-      num2 = num1;
-      rem = num2;
+    }
+    rem = num2 % num1;
+    num2 = num1;
+    num1 = rem;
+
   }
   printf("The GCF between the two numbers is %d", gcf);
 
