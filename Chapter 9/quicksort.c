fc8b490e09e982cd20e2ab0ca21dd52ed75ef3d6
Eric Hu <erichu03@gmail.com>
2020-04-26 16:46:57 -0400

    new files

---
+++ Chapter 9/quicksort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void quicksort(int a[], int low, int high);
+int split(int a[], int low, int high);
+
+int main (void) {
+  int i, num;
+
+  printf("Enter number of numbers to be sorted: ");
+  scanf("%d", &num);
+  int a[num];
+  printf("Enter %d numbers to be sorted: ", num);
+  for (i = 0; i < num; i++)
+    scanf("%d", &a[i]);
+  quicksort(a, 0, num - 1);
+
+  printf("In sorted order: ");
+  for (i = 0; i < num; i++)
+    printf("%d ", a[i]);
+  printf("\n");
+  return 0;
+  }
+  void quicksort(int a[], int low, int high) {
+    int middle;
+
+    if (low >= high) return;
+    middle = split(a, low, high);
+    quicksort(a, low, middle - 1);
+    quicksort(a, middle + 1, high);
+  }
+  int split (int a[], int low, int high) {
+    int part_element = a[low];
+    for (;;) {
+      while (low < high && part_element <= a[high])
+        high--;
+      if (low  >= high) break;
+      a[low++] = a[high];
+
+      while (low < high && a[low] <= part_element)
+        low++;
+      if (low >= high) break;
+      a[high--] = a[low];
+    }
+    a[high] = part_element;
+    return high;
+  }
---
+++ enums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+  enum Company xerox = XEROX;
+  enum Company google = GOOGLE;
+  enum Company ebay = EBAY;
+
+
+  printf("The value of xerox is %d\n", xerox);
+  printf("The value of google is %d\n", google);
+  printf("The value of ebay is %d", ebay);
+  return 0;
+}
