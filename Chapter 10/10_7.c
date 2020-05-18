271199a779e49bf90d5f0e0f3def67553e180241
Eric Hu <erichu03@gmail.com>
2020-05-17 23:38:12 -0400

    new file

---
+++ Chapter 10/10_7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+int num;
+bool grid;
+void display_num(void);
+void map_digits(void);
+int main() {
+  printf("Enter max amount of numbers for the display: ");
+  scanf("%d", &num);
+
+  bool grid[3][num * 3];
+
+
+  //printf("Enter a number: ");
+
+  return 0;
+}
+
+void display_num(void) {
+  for (int i = 0; i++; i < 3) {
+    for (int j = 0; j++; j < (sizeof(grid) / sizeof(grid[0]))) {
+
+    }
+  }
+}
