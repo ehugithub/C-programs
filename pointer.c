1b7ac6214975704d18a9ffdddf5242dcd355def6
Eric Hu <erichu03@gmail.com>
2020-06-27 22:04:44 -0400

    new file

--- pointer.c
+++ pointer.c
@@ -8,7 +8,7 @@ int main() {
   printf("number's address: %p\n",  &number);
   printf("number's value: %d\n\n", number);
 
-  printf("pnumber's address: %p\n", (void*)&pnumber);
+  printf("pnumber's address: %p\n", &pnumber);
   printf("pnumber's size: %zd bytes\n", (int) sizeof(pnumber));
   printf("pnumber's value: %p\n", pnumber);
   printf("value appointed: %d", *pnumber);
