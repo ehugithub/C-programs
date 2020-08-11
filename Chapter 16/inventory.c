2c839f3bb38512e9f4a48b6ae8cbd1f5f590d172
Eric Hu <erichu03@gmail.com>
2020-08-10 22:29:43 -0400

    new files

--- Chapter 16/inventory.c
+++ Chapter 16/inventory.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "readline.h"
+//#include "readline.h"
 #include "readline.c"
 #include <ctype.h>
 
--- Chapter 17/17_5.c
+++ Chapter 17/17_5.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
-#include "readline.h"
+#include <string.h>
+#include <stdlib.h>
 #include "readline.c"
-#include <ctype.h>
+
 #define MAX_LENGTH 20
 
 int read_line(char str[], int n);
-
-
+static int strcmp_void(const void *a, const void *b);
 
 int main () {
-  for(;;) {
+  char word[MAX_LENGTH + 1];
+  char *list[100];
+  int i;
+
+  for(i = 0; i < 4; i++) {
     printf("Enter word: ");
+    read_line(word, MAX_LENGTH);
 
-    char *words;
-    read_line(words, MAX_LENGTH);
-
+    /* if (getchar() == '\n')
+      break; */
+    printf("%d", strlen(word));
+    list[i] = (char *) malloc(strlen(word));
+    strcpy(list[i], word);
   }
+
+
+
+  qsort(list, i, sizeof(list[0]), strcmp_void);
+
+  for (int j = 0; j < i; j++)
+    printf( "%s ", list[j]);
   return 0;
+}
+
+static int strcmp_void(const void *a, const void *b)
+{
+    return strcmp(a, b);
 }
