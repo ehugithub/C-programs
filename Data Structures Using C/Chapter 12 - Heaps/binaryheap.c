2646255b9a065d3ecebc3c7dc76f86ef243dac5c
Eric Hu <erichu03@gmail.com>
2021-08-05 00:24:17 -0400

    heaps hard

--- Data Structures Using C/Chapter 12 - Heaps/binaryheap.c
+++ Data Structures Using C/Chapter 12 - Heaps/binaryheap.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 30
+//max heap
 
-struct node () {
-	int data;
-	struct node *left, *right;
-	
-} start* = NULL;
+int heap[SIZE];
+int index = 0;
 
-void insert(struct node *, int);
-void delete(struct node *);
-
+int parent(int);
+int left(int);
+int(right);
+void insert(int [], int, int *);
+void swap(int *, int *);
 
 int main () {
 
 	return 0;
+}
+
+void swap(int *a, int *n) {
+	int *temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+int parent(int n)
+	return (n + 1) / 2;
+
+int left(int n)
+	return (2 * n + 1);
+
+int right(int n)
+	return (2 * n + 2);
+
+int insert(int arr[], int data, int *n) {
+	arr[index] = data;
+	if(arr[index] > arr[parent(index)])
+		swap(arr[i], arr[parent(index)]);
+
 }
