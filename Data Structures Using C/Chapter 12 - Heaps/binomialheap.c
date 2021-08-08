aef79495b8b59833896207b01b566a139be9c5d2
Eric Hu <erichu03@gmail.com>
2021-08-08 00:43:52 -0400

    heap merge alog

--- Data Structures Using C/Chapter 12 - Heaps/binomialheap.c
+++ Data Structures Using C/Chapter 12 - Heaps/binomialheap.c
@@ -9,11 +9,10 @@ struct node {
 } *root;
 
 void create(struct node *);
-struct node *minkey();
-void linktree(struct node *, struct node *);
+struct node *minkey(struct node *);
 struct node *unionheap(struct node *, struct node *);
-
-
+struct node *merge(struct node *, struct node *);
+struct node *mindegree(struct node *);
 
 int main () {
 
@@ -24,8 +23,8 @@ void create(struct node *p) {
 	p = (struct node *)malloc(sizeof(struct node));
 }
 
-struct node *minkey() {
-	struct node *y = NULL, *x = root;
+struct node *minkey(struct node *p) {
+	struct node *y = NULL, *x = p;
 	int min = INT_MIN;
 	while(x != NULL) {
 		if(x->data < min) {
@@ -37,13 +36,38 @@ struct node *minkey() {
 	return y;
 }
 
-void linktree(struct node *p1, struct node *p2) {
-	p1->parent = p2;
-	p1->sibling = p2->child;
-	p2->child = p1;
-	p2->degree++;
+struct node *mindegree(struct node *p) {
+	struct node *n = NULL, *m = p;
+	int min = INT_MIN;
+	while(m != NULL) {
+		if(m->degree < min) {
+			min = m->degree;
+			n = m;
+			m->degree = INT_MAX;
+		}
+		m = m->sibling;
+	}
+	return (min == INT_MAX) ? NULL : n;
+}
+
+struct node *merge(struct node *n, struct node *k) {
+	// n is smaller than k
+	k->parent = n;
+	k->sibling = n->child;
+	n->child = k;
+	n->degree++;
+	return n;
 }
 
 struct node *unionheap(struct node *p1, struct node *p2) {
-
+	struct node *new, *temp = p1;
+	create(new);
+	new = (mindegree(p1) < mindegree(p2)) ? p1 : p2;
+	temp = new;
+	while(mindegree(p1) != NULL || mindegree(p2) != NULL) {
+		temp->sibling = (mindegree(p1) < mindegree(p2)) ? p1 : p2;
+		temp = temp->sibling;
+	}
+	free(p1);
+	free(p2);
 }
